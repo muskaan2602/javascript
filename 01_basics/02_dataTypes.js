c2410374e3a579ee1d343d73a137bc4d829924ff
muskaan2602 <muskaanbansal1202@gmail.com>
2025-07-26 06:38:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//  alert(3+3) // we are using node js not web broswer
+
+let name = "muskaan"
+let age = 20
+let isLoggedIn = true
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value //empty value hai //object hai ye ek
+//undefined => // value assign nai ki hai // ye ek datatype hai
+//symbol => unique value
+
+
+//object
+
+console.log(typeof name);
