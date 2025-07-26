dfddc26db8c62166b8868e215bc5363bd922f59e
muskaan2602 <muskaanbansal1202@gmail.com>
2025-07-26 07:34:21 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueinNum = Number(score)
+console.log(typeof valueinNum);
+
+
+// "33" => 33
+// "33abc" => NaN (not a number) datatype = number
+// true => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = true
+
+let booleanisloggedIn = Boolean(isLoggedIn)
+console.log(booleanisloggedIn);
+
+// 1 => true
+// 0 => false
+// " " => false
+// "abc" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(srtringnumber);
