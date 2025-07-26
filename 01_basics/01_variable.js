06328f9246a03909ff16d55964a77381863d05c0
muskaan2602 <muskaanbansal1202@gmail.com>
2025-07-26 06:12:58 +0000

    variabless

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "muskaan@gmail.com"
+var accountPassword = "123"
+accountCity = "chandigarh"
+
+//accountId = 244
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountId]);
