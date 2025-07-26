6f9989a7a22c3eda3b71c3581aaf0776e6f86bbb
muskaan2602 <muskaanbansal1202@gmail.com>
2025-07-26 10:46:59 +0000

    conversions2.0

---
+++ 01_basics/03_conversions2.0.js
@@ -0,0 +1,17 @@
+let value = 3
+let negvalue = -value
+
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2) // exponentiation
+console.log(2%2) // modulus
+
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1 + " " + str2
+console.log(str3)
