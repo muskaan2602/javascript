bd54a71f0ae4520cdea6e22cf9de78c6bb2dc381
muskaan2602 <muskaanbansal1202@gmail.com>
2025-08-16 08:39:00 +0000

    stringss

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,12 @@
+const name = "muskaan"
+const repoCount = 40
+ 
+console.log(`hello my name is ${name} and i have ${repoCount} repos`);
+
+const gameName = new String("ride or die")
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.__proto__);
